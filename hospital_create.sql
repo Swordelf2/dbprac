af6e6183cfd57da759bb4500cb3f9a63d8add727
Evgeny Papazyan <swordelf@mail.ru>
2018-10-04 18:57:08 +0300

    Started the work on creating the table creations script

--- hospital_create.sql
+++ hospital_create.sql
@@ -1,42 +1,52 @@
+-- Clean the database
+drop database hospital;
+create database hospital with TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+alter database hospital owner TO swordelf;
+\connect hospital
+
+-- "Create table" statements
 alter database hospital
     set datestyle to "ISO, DMY";
 
 set datestyle to "ISO, DMY";
 
-create table Лечение (
-    Лечение_ID integer primary key,
-    Пациент_ID integer,
-    Врач_ID integer,
-    Дата_начала date,
-    Дата_окончания date,
-    Диагноз varchar(300),
-    Состояние_ID integer
+create table Диагноз (
+    Диагноз_ID integer primary key,
+    Название varchar(30) not null,
+    Описание varchar(300)
 );
 
-create table Прием (
-    Прием_ID integer primary key,
-    Лечение_ID integer,
-    Кабинет integer,
-    Время_начала timestamp,
-    Время_окончания timestamp
+create table Специализация (
+    Специализация_ID integer primary key,
+    Название varchar(30) not null
+);
+
+create table Состояние (
+    Состояние_ID integer primary key,
+    Описание varchar(50) not null
+);
+
+create table Социальный_статус (
+    Социальный_статус_ID integer primary key,
+    Название varchar(30) not null
+);
+
+create table Вид_терапии (
+    Вид_терапии_ID integer primary key,
+    Название varchar(30),
+    Описание varchar(300)
 );
 
 create table Врач (
     Врач_ID integer primary key,
     Фамилия varchar(30),
     Имя varchar(15),
-    Отчество varchar(20)
-);
-
-create table Специализация (
-    Специализация_ID integer primary key,
-    Название varchar(30)
-);
-
-create table Врач_Специализация (
-    Врач_ID integer,
-    Специализация_ID integer,
-        primary key(Врач_ID, Специализация_ID)
+    Отчество varchar(20),
+    Стаж smallint,
+    Время_начала time,
+    Время_окончания time,
+    Кабинет smallint,
+        constraint check_time check (Время_начала <= Время_окончания)
 );
 
 create table Пациент (
@@ -45,20 +55,36 @@ create table Пациент (
     Фамилия varchar(30),
     Имя varchar(15),
     Отчество varchar(20),
-    Год_рождения smallint,
-    Социальный_статус_ID integer
+    Дата_рождения date not null,
+    Социальный_статус_ID integer,
+        constraint check_date check (Дата_рождения between
+                '1918-01-01' and current_date)
 );
 
-create table Состояние (
-    Состояние_ID integer primary key,
-    Описание varchar(50)
+create table Лечение (
+    Лечение_ID integer primary key,
+    Пациент_ID integer,
+    Диагноз_ID integer
 );
 
-create table Социальный_статус (
-    Социальный_статус_ID integer primary key,
-    Описание varchar(50)
+create table Терапия (
+    Лечение_ID integer,
+    Вид_терапии_ID integer,
+    Начало timestamp,
+    Окончание timestamp
 );
 
-alter table Лечение 
-    add foreign key (Пациент_ID)
-        references Пациент(Пациент_ID)
+create table Прием (
+    Прием_ID integer primary key,
+    Терапия_ID integer,
+    Врач_ID integer,
+    Время_начала timestamp,
+    Время_окончания timestamp
+);
+
+
+create table Врач_Специализация (
+    Врач_ID integer,
+    Специализация_ID integer,
+        primary key(Врач_ID, Специализация_ID)
+);
